63c4a8677e8d9214b7c51107c3336f7e38117e35
swaroop-chandra <swaroop.chandra666@gmail.com>
2021-03-23 13:37:07 +0530

    minor modification in showing pop ups

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import YouTube from 'react-youtube';
 import axios from './axios';
 import "./Row.css";
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl , isLargeRow}) {
 
   const [movies, setMovies] = useState([]);
+  const [trailerUrl ,setTrailerUrl] = useState("");
 
   // a snipet of code which runs based on the condition 
   useEffect(() => {
@@ -17,6 +20,27 @@ function Row({ title, fetchUrl , isLargeRow}) {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const handleClick = (movie) => {
+    if(trailerUrl) {
+      setTrailerUrl("");
+    }else {
+      movieTrailer(movie?.name || "")
+      .then((url) => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get("v"));
+      })
+      .catch(error => console.log(error) )
+    }
+  }
+
+  const opts = {
+    height : "390",
+    width : "100%",
+    playerVars : {
+      autoplay: 1,
+    } 
+  }
 
   // console.log(movies)
 
@@ -29,12 +53,12 @@ function Row({ title, fetchUrl , isLargeRow}) {
 
         {
           movies.map(movie => (
-           <img key={movie.id} className={`row_poster ${isLargeRow && "row_posterLarge"} `}  src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.original_title} />
+           <img key={movie.id} onClick={()=> handleClick(movie)} className={`row_poster ${isLargeRow && "row_posterLarge"} `}  src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.original_title} />
           ))
         }
 
       </div>
-
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/> }
     </div>
   )
 }
